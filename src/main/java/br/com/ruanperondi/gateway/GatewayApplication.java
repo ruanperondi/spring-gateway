e507a5be94e87256a0faf22186b84d41c466ca87
Ruan Perondi <ruan.c.perondi@gmail.com>
2021-03-10 00:40:45 -0400

    Remoção Zuul

--- src/main/java/br/com/ruanperondi/gateway/GatewayApplication.java
+++ src/main/java/br/com/ruanperondi/gateway/GatewayApplication.java
@@ -3,7 +3,6 @@ package br.com.ruanperondi.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
